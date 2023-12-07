16212f23c0a40fa116e9be645b63ea400c6228bf
lauhoyin3a <lauhoyin464866@gmail.com>
2023-12-07 11:43:10 +0800

    added backup function

--- excel_writer.py
+++ excel_writer.py
@@ -3,6 +3,8 @@ import pyautogui
 import pandas as pd
 import os
 import pyperclip
+import json
+import shutil
 
 vba_file_path=r'C:\Users\Tom\Downloads\VBA script.txt'
 with open(vba_file_path,'r') as file:
@@ -11,6 +13,12 @@ checking_path=r"C:\Users\Tom\Downloads\checking.txt"
 
 with open(checking_path,'r') as file:
     checking_text=file.read()
+
+excel_file_path_list=[]
+
+
+def copy_file(input_path,output_path):
+    shutil.copy(input_path,output_path)
 
 def pd_checking(excel_file_path,field_name, field_type):
     df=pd.read_excel(excel_file_path)
@@ -40,17 +48,9 @@ def paste_vba_script(vba_script,excel_file_path):
     time.sleep(1)
     pyautogui.moveTo(579,364)
     pyautogui.click()
-    print(vba_script)
     pyautogui.typewrite(vba_script,interval=0.0007)
     time.sleep(2)
-    # pyautogui.hotkey('ctrl','a')
-    # time.sleep(1)
-    # pyautogui.hotkey('ctrl','c')
-    # time.sleep(1)
-    # print(pyperclip.paste())
-    # if pyperclip.paste()==pyperclip.copy(checking_path):
-    #     print("same")
-    #time.sleep(10)
+
 def run_vba_script(field_name,column_name,value):
     time.sleep(1)
     pyautogui.hotkey('f5')
@@ -63,9 +63,9 @@ def run_vba_script(field_name,column_name,value):
     time.sleep(1)
     pyautogui.typewrite(value)
     pyautogui.press('enter')
-    time.sleep(2)
+
     pyautogui.press('enter')
-    time.sleep(1)
+
     pyautogui.press('enter')
     time.sleep(1)
     # pyautogui.hotkey('ctrl','s')
@@ -79,15 +79,14 @@ def exit_excel():
     pyautogui.hotkey('ctrl', 's')
     time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(1)
     pyautogui.press('enter')
-    time.sleep(2)
-    pyautogui.hotkey('alt', 'f4')
-
-
     time.sleep(1)
     pyautogui.hotkey('alt', 'f4')
-    time.sleep(2)
+
+
+
+    pyautogui.hotkey('alt', 'f4')
+    time.sleep(1)
 def read_csv_input(file_path):
     data = pd.read_excel(file_path)
     print(data.head())
@@ -101,6 +100,13 @@ def read_csv_input(file_path):
     print(field_list)
     print(value_list)
     return (name_list,field_list,value_list)
+
+def read_file_path(file_path):
+    f = open(file_path)
+    data = json.load(f)
+    for path in data['excel_file_path_list']:
+        excel_file_path_list.append(path)
+
 
 
 
@@ -117,20 +123,11 @@ if process == 'n':
     raise SystemExit
 
 
-excel_file_path_list=[]
-excel_file_path_list.append(r'C:\Users\Tom\ArcGIS\My Survey Designs\GOMPS_Event_Misc 122\GOMPS_Event_Misc 122.xlsx')
-#excel_file_path_list.append(r'C:\Users\Tom\ArcGIS\My Survey Designs\GOMPS_Typhoon_Shelter 3\GOMPS_Typhoon_Shelter 3.xlsx')
-
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\1f898ea71b894bedb43b3face5b23faa\GOMPS_Event_Misc.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\f646d8b9480549638137236afc417364\GOMPS_Typhoon_Shelter.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\f11f024cf8e8410bb1e0934ecc57e39c\GOPMS_Hopsital.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\d15fdf8320ae46c4bd286dcd2d988571\GOMPS_Restricted_Access.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\994e964647f14fa297eeb30e80f922a8\GOMPS_Railway_Development.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\71ec010703a14a8d855dda625f3ddc10\GOMPS_Dangerous_Goods.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\d7655e7434aa4f1ca9459828b1a2d041\GOPMS_Ambulance.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\d6de5b3acd7642668aa3e52307fbfe5a\GOMPS_Hotel.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\7ae799842cdb478da58f67ac43683c76\GOMPS_Tunnel.xlsx")
-# excel_file_path_list.append(r"C:\Users\Tom\ArcGIS\My Survey Designs\815511cb28dd480a99ca418c6a02149b\GOMPS_view_edit.xlsx")
+read_file_path('staging_survey.json')
+#read_file_path("test_survey.json")
+#for excel_file_path in excel_file_path_list:
+#      copy_file(excel_file_path,r"C:\Users\Tom\Downloads\survey_backup")
+# exit()
 for excel_file_path in excel_file_path_list:
     paste_vba_script(vba_script, excel_file_path)
     for i in range(len(name_list)):
@@ -141,4 +138,4 @@ for excel_file_path in excel_file_path_list:
             continue
 
         run_vba_script(name_list[i],field_list[i],value_list[i])
-    exit_excel();
+    exit_excel()
